59eb8c2ac783a3aeaf9a1d20b377143288c744de
ri <git@tnu.jp>
2015-04-17 11:00:20 +0900

    program impl++

--- lib/ronsen/program.rb
+++ lib/ronsen/program.rb
@@ -1,5 +1,31 @@
 module Ronsen
   class Program
+    class << self
+      def parse_entire_xml(xml_str)
+        doc = Nokogiri.parse(xml_str)
+        doc.css("program").map{|p| self.new(p)}
+      end
+    end
+
+    def initialize(prog_xml)
+      @xml = prog_xml
+    end
+
+    def id
+      @xml.attr("id").text
+    end
+
+    def original_xml
+      @xml.to_s
+    end
+
+    def as_hash
+    end
+
+    def download
+      target = @xml.css("program > movie_url").first.text
+      Accessor.instance.get_bin(target)
+    end
 
   end
 end
