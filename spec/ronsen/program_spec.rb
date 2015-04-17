398d17ad6fdd13b3e56ba48f9c13369ca4d6dee0
ri <git@tnu.jp>
2015-04-17 13:29:10 +0900

    program_spec++

--- spec/ronsen/program_spec.rb
+++ spec/ronsen/program_spec.rb
@@ -18,7 +18,31 @@ describe Ronsen::Program do
       }.to raise_error
     end
 
+  end
 
+  describe "#initialize" do
+    subject { -> { Ronsen::Program.new(xml) } }
+    context "arg is nil" do
+      let(:xml) { nil }
+      it { is_expected.to raise_error }
+    end
+
+    context "arg is string" do
+      let(:xml) { "<?xml>" }
+      it { is_expected.to raise_error }
+    end
+
+    context "arg not contains <program>" do
+      let(:xml) {
+        xml_str = <<-'EOF'
+<?xml version="1.0" encoding="UTF-8"?>
+<programs>
+</programs>
+        EOF
+        Nokogiri.parse(xml_str)
+      }
+      it { is_expected.to raise_error }
+    end
 
   end
 end
