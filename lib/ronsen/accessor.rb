f4ba22a06adc98d6f4a477ce70861b57eb1dfc06
ri <git@tnu.jp>
2015-04-17 10:40:44 +0900

    change onsen api (android)

--- lib/ronsen/accessor.rb
+++ lib/ronsen/accessor.rb
@@ -1,20 +1,19 @@
 module Ronsen
   class Accessor
     ONSEN_HOST = "http://onsen.ag"
+
     def initialize
       @conns = {}
-      @conns[ONSEN_HOST] = Faraday.new(url:ONSEN_HOST) {|f|
+    end
+
+    def get_programs_xml
+      conn = Faraday.new(url:ONSEN_HOST) {|f|
         f.request :url_encoded
         f.adapter Faraday.default_adapter
       }
-    end
 
-    def get_xml(week_no)
-      code = (Time.now.to_f*1000).to_i.to_s
-      file_name = "regular_#{week_no}"
-
-      res = @conns[ONSEN_HOST].post("/getXML.php",{code:code, file_name:file_name})
-      if res.status != 200
+      res = conn.get("/app/programs.xml")
+      if res.status != 200 || res.body.empty?
         raise
       end
       res.body
