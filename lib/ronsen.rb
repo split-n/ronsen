a1316bb837b8f1f2d80100fb4f05e3de955f2c9c
ri <git@tnu.jp>
2015-04-20 14:26:14 +0900

    require sha1

--- lib/ronsen.rb
+++ lib/ronsen.rb
@@ -4,6 +4,7 @@ require 'singleton'
 require 'open-uri'
 require 'active_support/core_ext/hash/conversions'
 require 'mp3info'
+require "digest/sha1"
 
 require "ronsen/version"
 require "ronsen/accessor"
