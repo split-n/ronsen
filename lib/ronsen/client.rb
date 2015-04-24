9d6810f1d947a49054e713b02b7295e3f1d145f1
ri <git@tnu.jp>
2015-04-24 11:57:31 +0900

    client++

--- lib/ronsen/client.rb
+++ lib/ronsen/client.rb
@@ -1,16 +1,15 @@
 module Ronsen
   class Client
+    def initialize
+      @accessor = Accessor.new
+    end
+
     def get_programs
-      Proram.parse_entire_xml(accessor.get_programs_xml)
+      Proram.parse_entire_xml(@accessor.get_programs_xml)
     end
 
     def user_agent=(str)
       @accessor.user_agent = str
-    end
-
-    private
-    def accessor
-      @accessor ||= Accessor.new
     end
   end
 end
