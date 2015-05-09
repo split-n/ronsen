d0d03cf2856a48c53ba649a91e1b05c331902463
ri <git@tnu.jp>
2015-05-10 06:57:13 +0900

    Add get_active_programs, rename get_all_programs

--- lib/ronsen/client.rb
+++ lib/ronsen/client.rb
@@ -4,7 +4,11 @@ module Ronsen
       @accessor = Accessor.new
     end
 
-    def get_programs
+    def get_active_programs
+      get_all_programs.select{|p| p.can_download?}
+    end
+
+    def get_all_programs
       Program.parse_entire_xml(@accessor.get_programs_xml,@accessor)
     end
 
