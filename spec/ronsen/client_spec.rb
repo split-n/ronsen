dce4899a005db9f5bb650211dfc0e7c4710d929b
ri <git@tnu.jp>
2015-05-10 08:30:40 +0900

    client_spec programs

--- spec/ronsen/client_spec.rb
+++ spec/ronsen/client_spec.rb
@@ -3,10 +3,52 @@ require 'spec_helper'
 describe Ronsen::Client do
   let(:accessor) { double("accessor mock") }
   let(:client) { Ronsen::Client.new(accessor) }
-  describe "#get_all_programs" do
+
+  context "with programs.xml" do
+    let(:programs_xml) {
+      xml_path = Pathname.new(__dir__) + "../fixtures/programs.xml"
+      xml_path.read
+    }
+
+    before do
+      allow(accessor).to receive(:get_programs_xml).and_return(programs_xml)
+    end
+
+    describe "#get_all_programs" do
+      let(:all_programs_count) { 81 }
+      subject { client.get_all_programs }
+
+      it { expect(subject.count).to eq all_programs_count }
+    end
+
+    describe "#get_active_programs" do
+      let(:active_programs_count) { 71 }
+      subject { client.get_active_programs }
+
+      it { expect(subject.count).to eq active_programs_count }
+    end
   end
 
-  describe "#get_active_programs" do
+  context "Bad connection" do
+    before do
+      allow(accessor).to receive(:get_programs_xml).and_raise(Ronsen::ConnectionError)
+    end
+
+    describe "#get_all_programs" do
+      subject { -> { client.get_all_programs } }
+
+      it "through error" do
+        is_expected.to raise_error Ronsen::ConnectionError
+      end
+    end
+
+    describe "#get_active_programs" do
+      subject { -> { client.get_active_programs } }
+
+      it "throwgh error" do
+        is_expected.to raise_error Ronsen::ConnectionError
+      end
+    end
 
   end
 
